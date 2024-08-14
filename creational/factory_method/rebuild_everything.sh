de4776e899c1fc128659d7e61f33b0d2806b5bda
Dylan Colli <dylanfrankcolli@gmail.com>
2024-08-14 11:28:05 -0400

    Fix previous merge carelessness

--- creational/factory_method/rebuild_everything.sh
+++ creational/factory_method/rebuild_everything.sh
@@ -14,11 +14,6 @@ if [ -f test_main ]
 then
   ./test_main
 else
-<<<<<<< HEAD
   export RED_FONT_COLOR='\033[0;31m'
   echo -e "${RED_FONT_COLOR}Build failed!"
 fi
-=======
-  echo "Build failed!"
-fi
->>>>>>> f227b0166a0dc51509b5228cf015c068b778c456
